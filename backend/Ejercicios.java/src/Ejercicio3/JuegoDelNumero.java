2f981b7fcc296a025a39ff1c426c77e395584884
Villasenorgu <alberto.villasenor-guzman@capgemini.com>
2022-01-29 21:10:23 +0100

    Ejercicio 3 con interfaz

--- backend/Ejercicios.java/src/Ejercicio3/JuegoDelNumero.java
+++ backend/Ejercicios.java/src/Ejercicio3/JuegoDelNumero.java
@@ -1,51 +1,46 @@
 package Ejercicio3;
 
 import java.util.Random;
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class JuegoDelNumero {
-	
+
 	public static void main(String[] args) {
-		
+
 		inicializar();
-		
+
 	}
-	
+
 	public static void inicializar() {
 		var rnd = new Random();
 		int num = rnd.nextInt(100);
-		System.out.println("Numero Secreto:" + num);		
+		System.out.println("Numero Secreto:" + num);
 		Jugada(num);
 	}
-	
-	public static String Jugada(int nRandom) {
-		
+
+	public static void Jugada(int nRandom) {
+		var respuesta = "";
 		for (int intentos = 9; intentos >= 0; intentos--) {
-			//JOptionPane.showInputDialog("Intenta adivinarlo, introduce un número:");
-			System.out.println("Intenta adivinarlo, introduce un número:");
-			Scanner teclado = new Scanner(System.in);
-			var cad = teclado.nextLine();
-			int numeroIntroducido = Integer.parseInt(cad);
+			if (respuesta.equals("")) {
+				respuesta = JOptionPane.showInputDialog("Intenta adivinarlo, introduce un número:");
+			}
+			int numeroIntroducido = Integer.parseInt(respuesta);
 
 			if (numeroIntroducido == nRandom) {
-				System.out.println("Has acertado!!");
+				JOptionPane.showMessageDialog(null, "Has acertado!!");
 				break;
 			} else {
-				if((numeroIntroducido != nRandom) && intentos == 0) {					
-				System.out.println("No has conseguido acertar el número");
-				return "No has conseguido acertar el número";
-				}else if (numeroIntroducido > nRandom) {					
-					System.out.println("El número que buscas es mas pequeño.Te quedan "+ intentos +" intentos");
-				}
-				else {
-					System.out.println("El número que buscas es mas grande.Te quedan "+ intentos +" intentos");
+				if ((numeroIntroducido != nRandom) && intentos == 0) {
+					JOptionPane.showMessageDialog(null, "No has conseguido acertar el número");
+				} else if (numeroIntroducido > nRandom) {
+					respuesta = JOptionPane
+							.showInputDialog("El número que buscas es mas pequeño.Te quedan " + intentos + " intentos");
+				} else {
+					respuesta = JOptionPane
+							.showInputDialog("El número que buscas es mas grande.Te quedan " + intentos + " intentos");
 				}
 			}
-
 		}
-
-		return "Has acertado";
 	}
 }
