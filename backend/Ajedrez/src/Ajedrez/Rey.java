4658b3b3bb0373f468c34b68a3444e57260ab891
Villasenorgu <alberto.villasenor-guzman@capgemini.com>
2022-02-10 09:09:28 +0100

    Ajedrez clase Rey

--- backend/Ajedrez/src/Ajedrez/Rey.java
+++ backend/Ajedrez/src/Ajedrez/Rey.java
@@ -4,8 +4,16 @@ public class Rey extends Pieza {
 	public Rey(Color color) {
 		super(color);
 	}
+
 	protected boolean esValido(Movimiento movimiento, Tablero tablero) {
-		return true;
+		if (movimiento.SaltoHorizaontal() <= 1 && movimiento.SaltoVertical() <= 1) {
+			if ((tablero.hayPieza(movimiento.getPosFin())
+					&& tablero.Escaque(movimiento.getPosFin()).getElColor() != this.getElColor())
+					|| !tablero.hayPieza(movimiento.getPosFin())) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
 }
