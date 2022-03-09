0dd9b58e99d74b6a4e7cdba7795900179332be5f
Villasenorgu <alberto.villasenor-guzman@capgemini.com>
2022-03-09 12:02:37 +0100

    sql

--- solucionesSQL.sql
+++ solucionesSQL.sql
@@ -4,7 +4,7 @@ SELECT * FROM actor WHERE first_name = 'NICK'
 
 ·Obtener una lista con todos los distritos distintos (district en address).
 
-SELECT district FROM address 
+SELECT DISTINCT district FROM address 
 WHERE district IS NOT NULL
 ORDER BY district
 
@@ -24,46 +24,66 @@ ORDER BY rental_rate/LENGTH DESC
 SELECT customer_id cliente, count(amount) numeroDeALquileres ,SUM(amount) total, AVG(amount) media
 FROM payment
 GROUP BY cliente
-HAVING count(amount) < 25 AND SUM(amount) > 100
+HAVING count(*) < 25 AND SUM(amount) > 100
+ORDER BY media DESC, total, numeroDeAlquileres;
 
 ·Obtener los 5 nombres de actor más repetidos (aprox).
 
-SELECT first_name , COUNT(first_name) repeticiones
-FROM customer
+SELECT first_name , COUNT(*) repeticiones
+FROM actor
 GROUP BY first_name
 ORDER BY repeticiones DESC
 LIMIT 5
 
 ·Para felicitar el año nuevo chino se necesita el listado con los datos postales de los clientes residentes en China y Taiwan.
 
-SELECT c.first_name, c.last_name, a.address, a.postal_code, ci.city, co.country
+SELECT CONCAT(c.first_name, ' ' ,c.last_name) nombre, a.address, a.postal_code, ci.city, co.country
 FROM customer AS c
-INNER JOIN
-(address AS a INNER JOIN city AS ci INNER JOIN country AS co ON ci.country_id = co.country_id ON a.city_id = ci.city_id)
-ON c.address_id = ci.city_id
+INNER JOIN address AS a 
+	ON c.address_id = a.address_id	
+INNER JOIN city AS ci 
+	ON a.city_id = ci.city_id
+INNER JOIN country AS co 
+	ON ci.country_id = co.country_id 
 WHERE co.country LIKE 'China' OR co.country LIKE 'Taiwan'
+ORDER BY nombre
 
 ·Mostrar los datos de las tiendas, conocidas por la ciudad y país donde están ubicadas, indicando el nombre del gerente, el numero de películas en inventario, el numero de títulos diferentes y el número de clientes atendidos.
 
-SELECT s.store_id Tienda, s.first_name Nombre, s.last_name Apellido, ci.city Ciudad, co.country Pais, count(inv.film_id) 'Nº de Peliculas', COUNT(DISTINCT inv.film_id) 'Peliculas Diferentes', COUNT(DISTINCT cus.store_id) 'Nº de Clientes'
+SELECT s.store_id Tienda, s.first_name Nombre, s.last_name Apellido, CONCAT(ci.city, ' ' ,co.country) tienda, count(inv.film_id) 'Nº de Peliculas', COUNT(DISTINCT inv.film_id) 'Peliculas Diferentes', COUNT(DISTINCT cus.store_id) 'Nº de Clientes'
 FROM staff AS s
-INNER JOIN
-(address AS a INNER JOIN city AS ci INNER JOIN country AS co ON ci.country_id = co.country_id ON a.city_id = ci.city_id)
-ON s.address_id = a.address_id
-INNER JOIN inventory AS inv ON s.store_id = inv.store_id
-INNER JOIN customer AS cus ON s.store_id = cus.store_id
+INNER JOIN address AS a 
+	ON s.address_id = a.address_id
+INNER JOIN city AS ci 
+	ON a.city_id = ci.city_id
+INNER JOIN country AS co 
+	ON ci.country_id = co.country_id 
+INNER JOIN inventory AS inv 
+	ON s.store_id = inv.store_id
+INNER JOIN customer AS cus 
+	ON s.store_id = cus.store_id
 GROUP BY s.staff_id
 
 ·Obtener el listado detallado de alquileres indicando el identificador de alquiler, el titulo alquilado, las fechas de alquiler, devolución y tiempo transcurrido, nombres del cliente (nombre con apellidos), empleado (nombre con apellidos) y tienda (ciudad, país).
 
-SELECT r.rental_id, fT.title Titulo, r.rental_date 'Fecha de alquiler', r.return_date 'Fecha de devolcuion', DATEDIFF(CURRENT_DATE,r.rental_date) AS tiempoAlquilado, cus.first_name 'Nombre de Cliente', cus.last_name 'Apellido de Cliente', sta.first_name 'Nombre Trabajador', sta.last_name 'Apellido Trabajador', ci.city 'Ciudad de la tienda', co.country 'Pais de la tienda'
+SELECT r.rental_id, fT.title Titulo, r.rental_date 'Fecha de alquiler', r.return_date 'Fecha de devolcuion', DATEDIFF(r.return_date,r.rental_date) AS tiempoAlquilado, cus.first_name 'Nombre de Cliente', cus.last_name 'Apellido de Cliente', sta.first_name 'Nombre Trabajador', sta.last_name 'Apellido Trabajador', ci.city 'Ciudad de la tienda', co.country 'Pais de la tienda'
 FROM rental AS r
-INNER JOIN
-(staff AS sta INNER JOIN address AS a INNER JOIN city AS ci INNER JOIN country AS co ON ci.country_id = co.country_id ON a.city_id = ci.city_id ON a.address_id = sta.address_id)
+INNER JOIN staff AS sta 
 ON r.staff_id = sta.staff_id
-INNER JOIN inventory AS inv INNER JOIN film_text AS fT ON fT.film_id = inv.film_id ON inv.inventory_id = r.inventory_id
-INNER JOIN customer AS cus ON r.customer_id = cus.customer_id
+INNER JOIN address AS a
+ON a.address_id = sta.address_id 
+INNER JOIN city AS ci 
+ON a.city_id = ci.city_id 
+INNER JOIN country AS co 
+ON ci.country_id = co.country_id 
+INNER JOIN inventory AS inv 
+INNER JOIN film_text AS fT 
+ON fT.film_id = inv.film_id 
+ON inv.inventory_id = r.inventory_id
+INNER JOIN customer AS cus 
+ON r.customer_id = cus.customer_id
 GROUP BY r.rental_id
+
 
 ·Generar la lista diaria de alquileres vencidos no devueltos para poder llamar a los clientes y pedirles que los devuelvan, para ello debe mostrar las fechas de alquiler y vencimiento, el teléfono y nombre del cliente, así como el titulo de la película, priorizando los que mas tiempo llevan vencidos.
 
@@ -80,9 +100,9 @@ ORDER BY tiempoAlquilado DESC, cus.first_name
 
 ·Elaborar el ranking de los países que más alquilan las películas de GINA DEGENERES.
 
-SELECT actor.actor_id,actor.first_name,actor.last_name, co.country 'Pais', count(r.rental_id) AS numeroDeAlquileres 
+SELECT RANK() OVER(ORDER BY COUNT(*) DESC) Ranking, actor.actor_id,actor.first_name,actor.last_name, co.country 'Pais', count(r.rental_id) AS numeroDeAlquileres 
 FROM actor 
 INNER JOIN film_actor AS fA INNER JOIN film AS f INNER JOIN inventory AS i INNER JOIN rental AS r INNER JOIN customer AS c INNER JOIN address AS ad INNER JOIN city AS ci INNER JOIN country AS co ON co.country_id=ci.country_id ON ci.city_id= ad.city_id ON ad.address_id=c.address_id ON c.customer_id=r.customer_id ON r.inventory_id=i.inventory_id ON i.film_id=f.film_id ON f.film_id = fA.film_id ON fA.actor_id = actor.actor_id
 GROUP BY actor.actor_id, co.country
-HAVING actor.first_name LIKE "GINA"
+HAVING actor.first_name LIKE "GINA" AND actor.last_name LIKE "DEGENERES"
 ORDER BY numeroDeAlquileres DESC
