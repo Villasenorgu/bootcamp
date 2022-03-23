b3778ffa30c5caa657433b852ad44268e032f40d
Villasenorgu <alberto.villasenor-guzman@capgemini.com>
2022-03-23 16:38:31 +0100

    Ejemplo1 con Angular

--- angular/src/app/demos/demos.component.ts
+++ angular/src/app/demos/demos.component.ts
@@ -11,9 +11,61 @@ import {
   styleUrls: ['./demos.component.css'],
 })
 export class DemosComponent implements OnInit, OnDestroy {
+  private nombre: string = 'mundo';
+  public listado = [
+    {id : 1,nombre:'Madrid'},
+    {id : 2,nombre:'barcelona'},
+    {id : 3,nombre:'MURCIA'},
+    {id : 4,nombre:'ciudad Real'},
+
+  ]
+  idProvincia = 3;
+
+  public resultado: string | null = null;
+
+  visible = true;
+  estetica = { importante: true, error : false, urgente : true }
+
+  fontSize = 24;
+
   private suscriptor: Unsubscribable | undefined;
 
-  constructor(public vm: NotificationService) {}
+  constructor(public vm: NotificationService) {
+  }
+
+  get Nombre(): string { return this.nombre; }
+  set Nombre(value : string) {
+    if(value === this.nombre) return;
+    this.nombre = value;
+  }
+
+  public saluda(): void {
+    this.resultado = `Hola ${this.nombre}`
+  }
+
+  public despide(): void {
+    this.resultado = `Adios ${this.nombre}`
+  }
+
+  public di(algo : string): void {
+    this.resultado = `Dice ${algo}`
+  }
+
+  cambia() {
+    this.visible = !this.visible;
+    this.estetica.importante = !this.estetica.importante;
+    this.estetica.error = !this.estetica.error;
+  }
+
+  calcula(a : number, b : number): number {
+    return a + b;
+  }
+
+  add(provincia : string) {
+    const id = this.listado.length +1;
+    this.listado.push({id, nombre : provincia});
+    this.idProvincia = id;
+  }
 
   ngOnDestroy(): void {
     if (this.suscriptor) {
